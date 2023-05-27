7577000aac8059621436f9a6fa8375d9f4fbf1fa
HammadKhan946 <114697447+HammadKhan946@users.noreply.github.com>
2023-05-27 08:02:48 +0500

    Delete dod.py

--- dod.py
+++
@@ -1,8 +0,0 @@
-import os,platform
-os.system('git pull')
- 
-MAFIA=platform.architecture()[0]
-if MAFIA=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif MAFIA=="64bit":
-    __import__("dod64")
